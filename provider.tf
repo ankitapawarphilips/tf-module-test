4cb08e198a37c126850edc9d72fa7d0e2030935e
ankitapawarphilips <ankita.pawar@philips.com>
2023-02-28 11:49:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,10 @@
 provider "cloudfoundry" {
-
     api_url = var.CF_API_URL
     user = var.CF_USER
     password = var.CF_PASSWORD
     skip_ssl_validation = true
     app_logs_max = 30
 }
-
 
 provider "aws" {
   shared_config_files      = ["$HOME/.aws/config"]
